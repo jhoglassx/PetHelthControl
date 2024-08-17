9cedf92273f598a4743876f8034080b9c6d5284c
Jhoglas Rocha <Jhoglas.Rocha-ext@ab-Inbev.com>
2024-08-17 18:20:42 -0300

    implement pipeline run instrument tests when submit code for pull request

--- infrastructure/src/test/java/com/jhoglas/infrastructure/ExampleUnitTest.kt
+++ infrastructure/src/test/java/com/jhoglas/infrastructure/ExampleUnitTest.kt
@@ -1,18 +1,17 @@
 package com.jhoglas.infrastructure
 
-import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Ignore
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@Ignore
 class ExampleUnitTest {
     @Test
-    @Ignore
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
